5180942b85456ad60a1c4c7fe391a61c148be3e3
Jared Fernandez <jared.fern@u.northwestern.edu>
2018-10-12 03:30:42 -0500

    LAB color features

--- color_extraction.py
+++ color_extraction.py
@@ -17,9 +17,22 @@ def pixel_cnt(img_path = "/home/jaredfern/Desktop/textures/curet-color", save=Tr
         
         for i in range(len(img_lab)):
             for j in range(len(img_lab[i])):
-                pixel_cnt[frozenset(img_lab[i][j])] += 1
+                pixel_cnt[tuple(img_lab[i][j])] += 1
         class_colors[int(texture_class)].append(pixel_cnt)
 
     if save: pickle.dump(class_colors, open("color_features.bin", "wb"))
     return class_colors
-            
+
+def extend_color_features(stsim_matrix, color_cnts, colors = 1, ordered = "L"):
+    for stsim_class in stsim_matrix.keys():
+        for img_ind in range(len(stsim_class)):
+            pixel_cnt = color_cnts[stsim_class][img_ind]
+            if ordered == "C": # Composition
+                color_features = [i[0] for i in pixel_cnt.most_common(colors)]
+            elif ordered == "L": # Luminance
+                color_features = sorted(pixel_cnt.keys(), key=lambda x:x[0])
+
+            flattened_colors = [val for color in color_features for val in color]
+            stsim_class[img_ind].extend(flattened_colors)                
+
+    return stsim_matrix
