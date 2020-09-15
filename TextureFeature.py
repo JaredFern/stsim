38aa2a2a12ef642d9934c264e321c007d0605e33
Jared Fernandez <jared.fern@u.northwestern.edu>
2020-09-15 14:27:48 -0400

    Add scale invariance param

--- TextureFeature.py
+++ TextureFeature.py
@@ -31,11 +31,13 @@ def _filter_class_counts(image_dir, class_cnt, fold_cnt, scale_invariance=False)
                 img_class_files[img_class].append(os.path.join(root, fpath))
     img_classes = set(img_class_files.keys())
     for img_class in img_classes:
-        if (len(img_class_files[img_class]) < fold_cnt) or (class_cnt > 0 and img_class in \
-                img_class_files and len(img_class_files[img_class]) < class_cnt):
+        if (len(img_class_files[img_class]) < fold_cnt) or (class_cnt > 0 and img_class in
+                                                            img_class_files and len(img_class_files[img_class]) < class_cnt):
             del img_class_files[img_class]
-    print(f"Filtered data to {len(img_class_files.keys())} classes of size {class_cnt}")
+    print(
+        f"Filtered data to {len(img_class_files.keys())} classes of size {class_cnt}")
     return img_class_files
+
 
 class TextureFeature(ABC):
     def __init__(self, image_dir_or_saved_bin, class_cnt, save_features, scale_invariance=False,
@@ -45,7 +47,7 @@ class TextureFeature(ABC):
         self.validator = StratifiedKFold(n_splits=fold_cnt, shuffle=True).split(
             self.features, self.labels)
 
-    def load_or_generate_features(self, image_dir_or_saved_features, class_cnt, fold_cnt, 
+    def load_or_generate_features(self, image_dir_or_saved_features, class_cnt, fold_cnt,
                                   scale_invariance, save_features):
         if os.path.isfile(image_dir_or_saved_features):
             return np.load(image_dir_or_saved_features)
@@ -86,43 +88,51 @@ class Ssim(TextureFeature):
     def generate_feature(self, fpath):
         source_image = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
         mu = metric.conv(source_image, self.window)
-        sigma_squared = metric.conv(source_image * source_image, self.window) - mu ** 2
+        sigma_squared = metric.conv(
+            source_image * source_image, self.window) - mu ** 2
         return mu, sigma_squared, fpath
 
     def compute_similarity(self, feature_1, feature_2):
         img1 = cv2.imread(feature_1[2], cv2.IMREAD_GRAYSCALE).astype(float)
         img2 = cv2.imread(feature_2[2], cv2.IMREAD_GRAYSCALE).astype(float)
-        sigma_12 = metric.conv(img1 * img2, self.window) - feature_1[0] * feature_2[0]
+        sigma_12 = metric.conv(img1 * img2, self.window) - \
+            feature_1[0] * feature_2[0]
         ssim_map = ((2*feature_1[0] * feature_2[0] + self.c1) * (2*sigma_12 + self.c2)) / (
-                (feature_1[0] ** 2 + feature_2[0] ** 2 + self.c1) *
-                (feature_1[1] + feature_2[1] + self.c2))
+            (feature_1[0] ** 2 + feature_2[0] ** 2 + self.c1) *
+            (feature_1[1] + feature_2[1] + self.c2))
         return ssim_map.mean()
+
 
 class Stsim1(TextureFeature):
     def generate_feature(self, fpath):
         steerable_filter = filterbank.Steerable()
         source_image = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
-        steerable_pyramid = steerable_filter.getlist(steerable_filter.buildSCFpyr(source_image))
+        steerable_pyramid = steerable_filter.getlist(
+            steerable_filter.buildSCFpyr(source_image))
         return steerable_pyramid
 
     def compute_similarity(self, feature_1, feature_2):
         img1 = cv2.imread(feature_1[2], cv2.IMREAD_GRAYSCALE).astype(float)
         img2 = cv2.imread(feature_2[2], cv2.IMREAD_GRAYSCALE).astype(float)
-        sigma_12 = metric.conv(img1 * img2, self.window) - feature_1[0] * feature_2[0]
+        sigma_12 = metric.conv(img1 * img2, self.window) - \
+            feature_1[0] * feature_2[0]
         ssim_map = ((2*feature_1[0] * feature_2[0] + self.c1) * (2*sigma_12 + self.c2)) / (
-                (feature_1[0] ** 2 + feature_2[0] ** 2 + self.c1) *
-                (feature_1[1] + feature_2[1] + self.c2))
+            (feature_1[0] ** 2 + feature_2[0] ** 2 + self.c1) *
+            (feature_1[1] + feature_2[1] + self.c2))
         return ssim_map.mean()
+
 
 class Stsim1(TextureFeature):
     def generate_feature(self, fpath):
         steerable_filter = filterbank.Steerable()
         source_image = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
-        steerable_pyramid = steerable_filter.getlist(steerable_filter.buildSCFpyr(source_image))
+        steerable_pyramid = steerable_filter.getlist(
+            steerable_filter.buildSCFpyr(source_image))
         return steerable_pyramid
 
     def compute_similarity(self, feature_1, feature_2):
-        np.mean([metric.pooling(feature_1[i], feature_2[i]) for i in range(len(feature_1))])
+        np.mean([metric.pooling(feature_1[i], feature_2[i])
+                 for i in range(len(feature_1))])
 
 
 class Stsim2(TextureFeature):
@@ -130,15 +140,18 @@ class Stsim2(TextureFeature):
         source_image = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
 
         steerable_filter = filterbank.Steerable()
-        steerable_pyramid = steerable_filter.getlist(steerable_filter.buildSCFpyr(source_image))
+        steerable_pyramid = steerable_filter.getlist(
+            steerable_filter.buildSCFpyr(source_image))
 
         steerable_filter_nosubbands = filterbank.SteerableNoSub()
-        steerable_crossterms = steerable_filter_nosubbands.buildSCFpyr(source_image)
+        steerable_crossterms = steerable_filter_nosubbands.buildSCFpyr(
+            source_image)
         return steerable_pyramid, steerable_crossterms
 
     def compute_similarity(self, feature_1, feature_2):
         # Base STSIM-1 pooled metrics
-        stsim2 = [metric.pooling(feature_1[0][i], feature_2[0][i]) for i in range(len(feature_1[0]))]
+        stsim2 = [metric.pooling(feature_1[0][i], feature_2[0][i])
+                  for i in range(len(feature_1[0]))]
 
         # Across scale, same orientation
         for scale in range(2, len(feature_1[1]) - 1):
@@ -148,7 +161,8 @@ class Stsim2(TextureFeature):
 
                 im21 = np.abs(feature_2[1][scale - 1][orient])
                 im22 = np.abs(feature_2[1][scale][orient])
-                stsim2.append(metric.compute_cross_term(im11, im12, im21, im22, 7).mean())
+                stsim2.append(metric.compute_cross_term(
+                    im11, im12, im21, im22, 7).mean())
 
         # Across orientation, same scale
         for scale in range(2, len(feature_1[1]) - 1):
@@ -159,12 +173,13 @@ class Stsim2(TextureFeature):
                 for orient2 in range(orient + 1, len(feature_1[1][1])):
                     im13 = np.abs(feature_1[1][scale][orient2])
                     im23 = np.abs(feature_2[1][scale][orient2])
-                    stsim2.append(metric.compute_cross_term(im11, im13, im21, im23, 7).mean())
+                    stsim2.append(metric.compute_cross_term(
+                        im11, im13, im21, im23, 7).mean())
         return np.mean(stsim2)
 
 
 class StsimC(TextureFeature):
-    def __init__(self, image_dir_or_saved_bin, class_cnt, save_features, scale_invariance, 
+    def __init__(self, image_dir_or_saved_bin, class_cnt, save_features, scale_invariance,
                  fold_cnt=5, mahalanobis_file=None, mahalanobis_type="cov", scope="intraclass",
                  aca_color_cnt=0, color_dir=""):
         self.mahalanobis_file = mahalanobis_file
@@ -180,7 +195,8 @@ class StsimC(TextureFeature):
         return mahalanobis(feature_1, feature_2, inv(self.mahalanobis_matrix))
 
     def generate_feature(self, fpath):
-        vec = list(metric.Metric().STSIM_M(cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)))
+        vec = list(metric.Metric().STSIM_M(
+            cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)))
         if self.aca_color_cnt:
             pixel_cnt = Counter()
             base_name = os.path.splitext(os.path.basename(fpath))[0]
@@ -190,11 +206,13 @@ class StsimC(TextureFeature):
                 for j in range(len(img_lab[i])):
                     pixel_cnt[tuple(img_lab[i][j])] += 1
 
-            color_features = [i[0] for i in pixel_cnt.most_common(self.aca_color_cnt)]
+            color_features = [i[0]
+                              for i in pixel_cnt.most_common(self.aca_color_cnt)]
             if len(color_features) < self.aca_color_cnt:
                 color_features.extend(
                     [(0, 0, 0) for i in range(self.aca_color_cnt - len(color_features))])
-            color_features = [val for color_idx in color_features for val in color_idx]
+            color_features = [
+                val for color_idx in color_features for val in color_idx]
             vec += color_features
         return vec
 
@@ -223,21 +241,25 @@ class StsimC(TextureFeature):
         if self.scope == 'stsim_i':
             train_features = []
             for train_idx in train_split:
-                normalized_feature = self.features[train_idx] - class_means[self.labels[train_idx]]
+                normalized_feature = self.features[train_idx] - \
+                    class_means[self.labels[train_idx]]
                 train_features.append(normalized_feature)
 
         # Flatten training array for computing global M matrix for
         if self.scope in ('global', 'stsim_i'):
             if self.mahalanobis_type == 'std':
-                self.mahalanobis_matrix = np.diag(np.std(train_features, axis=0))
+                self.mahalanobis_matrix = np.diag(
+                    np.std(train_features, axis=0))
             elif self.mahalanobis_type == 'var':
-                self.mahalanobis_matrix = np.diag(np.var(train_features, axis=0))
+                self.mahalanobis_matrix = np.diag(
+                    np.var(train_features, axis=0))
             elif self.mahalanobis_type == 'cov':
                 self.mahalanobis_matrix = np.cov(train_features, rowvar=False)
         # Compute single M matrix from individual class M matrices
         elif self.scope in ['intraclass', 'cluster']:
             if self.mahalanobis_type in ['std', 'var']:
-                self.mahalanobis_matrix = np.diag(np.mean(class_matrix, axis=0))
+                self.mahalanobis_matrix = np.diag(
+                    np.mean(class_matrix, axis=0))
             elif self.mahalanobis_type == 'cov':
                 self.mahalanobis_matrix = np.mean(class_matrix, axis=0)
         if self.mahalanobis_file:
@@ -245,7 +267,7 @@ class StsimC(TextureFeature):
 
 
 class LocalBinaryPattern(TextureFeature):
-    def __init__(self, image_dir_or_saved_bin, class_cnt, save_features, scale_invariance, 
+    def __init__(self, image_dir_or_saved_bin, class_cnt, save_features, scale_invariance,
                  fold_cnt, n_points, radius, lbp_method):
         self.n_points = n_points
         self.radius = radius
@@ -258,7 +280,9 @@ class LocalBinaryPattern(TextureFeature):
     def generate_feature(self, image_path):
         n_bins = 0
         img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        lbp = local_binary_pattern(img, self.n_points, self.radius, method=self.lbp_method)
+        lbp = local_binary_pattern(
+            img, self.n_points, self.radius, method=self.lbp_method)
         n_bins = max(int(lbp.max() + 1), n_bins)
-        lbp_hist = np.histogram(lbp, density=True, bins=n_bins, range=(0, n_bins))[0]
+        lbp_hist = np.histogram(
+            lbp, density=True, bins=n_bins, range=(0, n_bins))[0]
         return lbp_hist
